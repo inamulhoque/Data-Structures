cb13b04535558a2ff8f049ab4f3dd082bc926823
Inamul <inamulhoque097@gmail.com>
2022-06-15 21:30:43 +0530

    Added: Insert data in backward position

--- src/com/inamul/MyLinkedList.java
+++ src/com/inamul/MyLinkedList.java
@@ -2,15 +2,15 @@ package com.inamul;
 
 public class MyLinkedList extends LLFunctions{
     public static void main(String[] args) {
-        insertFirst(70);
-        printData();
-        System.out.println("\n");
-
-        insertFirst(30);
-        printData();
-        System.out.println("\n");
-
         insertFirst(56);
+        printData();
+        System.out.println("\n");
+
+        insertLast(30);
+        printData();
+        System.out.println("\n");
+
+        insertLast(70);
         printData();
     }
 }
