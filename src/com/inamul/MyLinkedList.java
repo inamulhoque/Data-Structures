5fa8c943460298c62c5ccdd56e52eeabda9acd4a
Inamul <inamulhoque097@gmail.com>
2022-06-16 14:44:09 +0530

    Added: Sorted the linkedlist

--- src/com/inamul/MyLinkedList.java
+++ src/com/inamul/MyLinkedList.java
@@ -1,23 +1,23 @@
 package com.inamul;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class MyLinkedList extends LLFunctions{
     public static void main(String[] args) {
-        insertFirst(56);
-        insertLast(30);
-        insertLast(70);
-        printData();
-        searchNode(30);
-        insertInBetween(30,40);
-        printData();
-        System.out.println("\n");
-        deleteAnyNode(40);
-        printData();
         LinkedList<Integer> newLL = new LinkedList();
         newLL.add(56);
         newLL.add(30);
+        newLL.add(40);
         newLL.add(70);
-        System.out.println("\nSize:"+newLL.size());
+        System.out.println("Before sort:");
+        for (Integer i:newLL) {
+            System.out.println("Data-> "+i+" ");
+        }
+        Collections.sort(newLL);
+        System.out.println("After sort:");
+        for (Integer i:newLL) {
+            System.out.println("Data-> "+i+" ");
+        }
     }
 }
